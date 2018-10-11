061f6276b10325db949640998faa151829c8e66f
pliantmeerkat <jackthomasbranch@yahoo.com>
2018-10-11 14:14:05 +0100

    updated lib, moved functions from test file to lib class, updated main

---
+++ src/test/e2e/src/main/java/ECSTest/Testing/App.java
@@ -0,0 +1,10 @@
+package ECSTest.Testing;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.setProperty("webdriver.gecko.driver", "E://Program Files//selenium-java-3.14.0/geckodriver.exe");
+    }
+}
